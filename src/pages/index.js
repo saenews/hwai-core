edba5d0a05b172cbc83b4225fdceca378fbfae8e
Dheeraj M Pai <29203967+dheerajmpai@users.noreply.github.com>
2019-12-19 02:21:00 +0530

    file change

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,12 +19,12 @@ export default () => (
             Hyperweb AI
           </h1>
           <p className="text-xl lg:text-2xl mt-6 font-light">
-            Fastest Websites ever!!
+            Build Blazing fase websites with AI. No coding required
           </p>
           <p className="mt-8 md:mt-12">
-            <Button size="lg">Get Started</Button>
+            <Button size="lg">Sign Up</Button>
           </p>
-          <p className="mt-4 text-gray-600">Sed fermentum felis ut cursu</p>
+          <p className="mt-4 text-gray-600"> Upload Images, Added content and let AI do the rest for you</p>
         </div>
         <div className="lg:w-1/2">
           <HeroImage />
@@ -33,32 +33,30 @@ export default () => (
     </section>
     <section id="features" className="py-20 lg:pb-40 lg:pt-48">
       <div className="container mx-auto text-center">
-        <h2 className="text-3xl lg:text-5xl font-semibold">Main Features</h2>
+        <h2 className="text-3xl lg:text-5xl font-semibold">Key Features</h2>
         <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
           <div className="flex-1 px-3">
             <Card className="mb-8">
-              <p className="font-semibold text-xl">Service One</p>
+              <p className="font-semibold text-xl">Blazing Fast</p>
               <p className="mt-4">
-                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
+                 Build Blazing Fast Websites built with the technologies used by Facebook, Twitter and Instagram. Hyperweb AI uses Reactjs and Gatsbyjs to build blazng fast websites. Typically Nx times faster than a wordpress website. 
                 velna vitae auctor integer.
               </p>
             </Card>
           </div>
           <div className="flex-1 px-3">
             <Card className="mb-8">
-              <p className="font-semibold text-xl">Service Two</p>
+              <p className="font-semibold text-xl">Secure</p>
               <p className="mt-4">
-                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                velna vitae auctor integer.
+                Why is it secure? Because it is static
               </p>
             </Card>
           </div>
           <div className="flex-1 px-3">
             <Card className="mb-8">
-              <p className="font-semibold text-xl">Service Three</p>
+              <p className="font-semibold text-xl">Automatic</p>
               <p className="mt-4">
-                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                velna vitae auctor integer.
+                Use AI to build Stuffs. No joke
               </p>
             </Card>
           </div>
@@ -71,8 +69,8 @@ export default () => (
         <div className="lg:pr-32 xl:pr-48">
           <h3 className="text-3xl font-semibold leading-tight">Market Analysis</h3>
           <p className="mt-8 text-xl font-light leading-relaxed">
-            Our team of enthusiastic marketers will analyse and evaluate how your company stacks
-            against the closest competitors
+            55% of wesites are built with slow and useless softwares. Get a competitive advantage by using Gatsbyjs that makes your websites 45x faster.
+            Google ranks you at the top.
           </p>
         </div>
       }
@@ -109,7 +107,7 @@ export default () => (
     />
     <section id="stats" className="py-20 lg:pt-32">
       <div className="container mx-auto text-center">
-        <LabelText className="text-gray-600">Our customers get results</LabelText>
+        <LabelText className="text-gray-600">Extremely Secure.</LabelText>
         <div className="flex flex-col sm:flex-row mt-8 lg:px-24">
           <div className="w-full sm:w-1/3">
             <StatsBox primaryText="+100%" secondaryText="Stats Information" />
